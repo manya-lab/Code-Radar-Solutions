a4ddae4e1750186914e75a7db221951b7e2e9732
manya-lab <ahujamanya753@gmail.com>
2025-02-28 18:30:04 +0530

    Solution to Print Floyd's Triangle

--- Print Floyd's Triangle@@67877e1818d53eb724f435c7/code.c
+++ Print Floyd's Triangle@@67877e1818d53eb724f435c7/code.c
@@ -3,11 +3,10 @@ int main(){
     int n;
     scanf("%d",&n);
     for(int i=1;i<=n;i++){
-        for(int j=0;j<=i;j++)
-            
-        
-        
+        for(int j=0;j<=i;j++){
+            printf("%d ",j+1);
+        }    
     }
-    printf("%d",j+1);
+    
     return 0;
 }
