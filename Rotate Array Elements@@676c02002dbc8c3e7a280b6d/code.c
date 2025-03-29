11ffa8e351e4c2892d60e183b15ac25cc773ef0b
manya-lab <ahujamanya753@gmail.com>
2025-03-29 17:47:56 +0530

    Solution to Rotate Array Elements

--- Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
+++ Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
@@ -1,38 +1,37 @@
 #include<stdio.h>
 
-void rotateLeft(int arr[], int n, int d) {
+void bringLastToFirst(int arr[], int n, int d) {
     int temp[d];
-    
-    // Store the first 'd' elements in a temporary array
+
+    // Store the last 'd' elements in a temporary array
     for (int i = 0; i < d; i++) {
-        temp[i] = arr[i];
+        temp[i] = arr[n - d + i];
     }
 
-    // Shift the remaining elements to the left
-    for (int i = 0; i < n - d; i++) {
-        arr[i] = arr[i + d];
+    // Shift the rest of the elements to the right
+    for (int i = n - 1; i >= d; i--) {
+        arr[i] = arr[i - d];
     }
 
-    // Move the temporary elements to the end
+    // Place the last 'd' elements at the beginning
     for (int i = 0; i < d; i++) {
-        arr[n - d + i] = temp[i];
+        arr[i] = temp[i];
     }
 }
 
 int main() {
     int n, d;
-    scanf("%d %d", &n, &d); // Input size of array and number of positions to rotate
+    scanf("%d %d", &n, &d); // Input array size and number of elements to bring to the front
     int arr[n];
     
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]); // Input array elements
     }
 
-    rotateLeft(arr, n, d);
-
+    bringLastToFirst(arr, n, d);
 
     for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]); // Print the rotated array
+        printf("%d ", arr[i]);
     }
 
     return 0;
